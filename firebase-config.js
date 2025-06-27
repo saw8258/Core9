64ac7d41fdc0ba659c3a13579787b1a44a7c0c31
saw8258 <mhtett55@gmail.com>
2025-06-27 14:50:01 +0800

    Add files via upload
     Add core source files for build

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAaMGewA6XfcJ5_plWhbAKMU_GDKxxSY2g",
+  authDomain: "core9-b77b5.firebaseapp.com",
+  projectId: "core9-b77b5",
+  storageBucket: "core9-b77b5.firebasestorage.app",
+  messagingSenderId: "292979889874",
+  appId: "1:292979889874:web:d97b016e847397f6397f0b"
+};
